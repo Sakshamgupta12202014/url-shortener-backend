4e53de97ec49c50ece4eecf68a912cd40f977ff3
Sakshamgupta12202014 <gupta.saksham0401@gmail.com>
2025-08-04 11:50:46 +0530

    updated origin url in CORS

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://url-shortener-frontend-xwl3.onrender.com",
     credentials: true, // allows cookies sent by frontend
   })
 );
